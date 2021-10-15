b3aff2e80b5d1e686657365ebb59c30288a4c17d
Szymon Tadrzak <szymontadrzak@gmail.com>
2021-10-15 18:56:23 +0100

    Initial Commit

---
+++ Pomodoro/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pomodoro
+//
+//  Created by Szymon Tadrzak on 15/10/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
